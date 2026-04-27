133c81c674b24a837bf05fab016c5c59ea8c7c02
Sandino Nunez <sandinosaso@gmail.com>
2026-04-27 17:27:19 +0200

    Prevent conflict of dependencies

--- model/setup.py
+++ model/setup.py
@@ -5,10 +5,5 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    python_requires=">=3.9",
-    install_requires=[
-        "tensorflow>=2.13",
-        "numpy>=1.24",
-        "python-dotenv>=1.0",
-    ],
+    python_requires=">=3.9"
 )
